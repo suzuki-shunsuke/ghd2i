fc5a1b701032f3e98df275fadf83430be94e7026
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2024-12-04 08:00:51 +0900

    feat: paginate discussions (#40)
    * feat: paginate discussions
    
    * fix: use cursor in query
    
    * fix: define variable $cursor

--- pkg/github/search.go
+++ pkg/github/search.go
@@ -16,10 +16,16 @@ query($query: String!) {
 type SearchQuery struct {
 	Search struct {
 		DiscussionCount int
+		PageInfo        *PageInfo
 		Nodes           []struct {
 			Discussion struct {
 				URL string
 			} `graphql:"... on Discussion"`
 		}
-	} `graphql:"search(type:DISCUSSION, query: $query, first:100)"`
+	} `graphql:"search(type:DISCUSSION, query: $query, first:100, after: $cursor)"`
+}
+
+type PageInfo struct {
+	HasNextPage bool
+	EndCursor   string
 }
