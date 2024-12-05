4beb7634dc0f67e85c6f33bc7ff7345ab2e8a94f
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2024-12-05 19:59:13 +0900

    feat: paginate discussion reactions (#48)

--- pkg/github/get_discussion.go
+++ pkg/github/get_discussion.go
@@ -27,10 +27,19 @@ func (c *Client) GetDiscussion(ctx context.Context, owner, name string, number i
 			q.Repository.Discussion.Comments.Nodes = append(q.Repository.Discussion.Comments.Nodes, comments...)
 		}
 	}
+	if q.Repository.Discussion.Reactions.PageInfo.HasNextPage {
+		for range 10 {
+			reactions, err := c.SearchDiscussionReactions(ctx, owner, name, number, q.Repository.Discussion.Comments.PageInfo.EndCursor)
+			if err != nil {
+				return nil, fmt.Errorf("search discussion reactions by GitHub GraphQL API: %w", err)
+			}
+			q.Repository.Discussion.Reactions.Nodes = append(q.Repository.Discussion.Reactions.Nodes, reactions...)
+		}
+	}
 	return q.Repository.Discussion, nil
 }
 
-func (c *Client) SearchComments(ctx context.Context, owner, name string, number int, cursor string) ([]*Comment, error) {
+func (c *Client) SearchComments(ctx context.Context, owner, name string, number int, cursor string) ([]*Comment, error) { //nolint:dupl
 	var comments []*Comment
 	variables := map[string]any{
 		"repoOwner": githubv4.String(owner),
@@ -72,4 +81,26 @@ func (c *Client) SearchDiscussions(ctx context.Context, query string) ([]string,
 		variables["cursor"] = q.Search.PageInfo.EndCursor
 	}
 	return urls, nil
+}
+
+func (c *Client) SearchDiscussionReactions(ctx context.Context, owner, name string, number int, cursor string) ([]*Reaction, error) { //nolint:dupl
+	var reactions []*Reaction
+	variables := map[string]any{
+		"repoOwner": githubv4.String(owner),
+		"repoName":  githubv4.String(name),
+		"number":    githubv4.Int(number), //nolint:gosec
+		"cursor":    cursor,
+	}
+	for range 10 {
+		q := &SearchDiscussionReactionsQuery{}
+		if err := c.v4Client.Query(ctx, q, variables); err != nil {
+			return nil, fmt.Errorf("search discussion reactions by GitHub GraphQL API: %w", err)
+		}
+		reactions = append(reactions, q.Repository.Discussion.Reactions.Nodes...)
+		if !q.Repository.Discussion.Reactions.PageInfo.HasNextPage {
+			return reactions, nil
+		}
+		variables["cursor"] = q.Repository.Discussion.Reactions.PageInfo.HasNextPage
+	}
+	return nil, nil
 }
